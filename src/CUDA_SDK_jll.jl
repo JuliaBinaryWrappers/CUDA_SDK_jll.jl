f3480f6517ec60bc00da34dd6f01c2596883b73c
jlbuild <juliabuildbot@gmail.com>
2023-08-25 12:19:35 +0000

    CUDA_SDK_jll build 12.2.1+0

---
+++ src/CUDA_SDK_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule CUDA_SDK_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("CUDA_SDK")
+JLLWrappers.@generate_main_file("CUDA_SDK", UUID("6cbf2f2e-7e60-5632-ac76-dca2274e0be0"))
+end  # module CUDA_SDK_jll
