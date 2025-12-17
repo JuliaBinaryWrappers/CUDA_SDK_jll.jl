389ca7ba13c7aed4901a8b89246a99e9cebb01e5
jlbuild <juliabuildbot@gmail.com>
2025-12-17 07:29:52 +0000

    CUDA_SDK_jll build 13.0.2+2

--- src/CUDA_SDK_jll.jl
+++ src/CUDA_SDK_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CUDA_SDK")
-JLLWrappers.@generate_main_file("CUDA_SDK", UUID("6cbf2f2e-7e60-5632-ac76-dca2274e0be0"))
+JLLWrappers.@generate_main_file("CUDA_SDK", Base.UUID("6cbf2f2e-7e60-5632-ac76-dca2274e0be0"))
 end  # module CUDA_SDK_jll
